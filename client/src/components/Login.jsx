4844de6a917d181e4332cf24b0cc78a3833e353b
prathameshdadgade <125120218+prathameshdadgade@users.noreply.github.com>
2024-12-04 19:38:19 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post('http://localhost:5500/api/user/login', formData)
+      .post('https://meshoo-mern.onrender.com/api/user/login', formData)
       .then((res) => {
         const { user } = res.data;
         localStorage.setItem('isLoggedIn', 'true'); // Set login session
