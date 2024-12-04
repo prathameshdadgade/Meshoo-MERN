d14014e08e2d807feb459c167f36ed8981cd05b8
prathameshdadgade <125120218+prathameshdadgade@users.noreply.github.com>
2024-12-04 19:03:02 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,12 +16,13 @@ app.use(cors());
 app.use('/api', Router);
 
 // Connect to MongoDB using the environment variable
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
 
 // Start the server using the PORT from environment variable
 const PORT = process.env.PORT || 5500;
