733b985a552792873e005fbf37274dadae1542d7
prathameshdadgade <125120218+prathameshdadgade@users.noreply.github.com>
2024-12-28 08:17:47 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,12 @@ import Header from './components/Header';
 import LowestPrices from './components/LowestPrices';
 import Signup from './components/Signup';
 import Login from './components/Login';
-
+import Cart from './components/Cart';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 function App() {
   return (
+    <Provider store={store}>
     <Router>
       <div className="App">
         {/* Header and Navigation */}
@@ -26,13 +29,14 @@ function App() {
               <ProductsForYou />
             </>
           } />
-
+          <Route path="/cart" element={<Cart />} />
           {/* Signup and Login Routes */}
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
+  </Provider>
   );
 }
 
