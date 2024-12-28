fde54350432b957cca72655e66c872a7672eb169
prathameshdadgade <125120218+prathameshdadgade@users.noreply.github.com>
2024-12-28 08:57:17 +0530

    Update Cart.jsx

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeFromCart } from '../redux/actions/cartActions';
-
+import './Cart.css';
 const Cart = () => {
     const cartItems = useSelector((state) => state.cart.cart);
     const dispatch = useDispatch();
